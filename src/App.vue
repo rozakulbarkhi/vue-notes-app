2fd140862298f1bc8f8819c3bf82bf58cf5009f5
Rozakul Barkhi <rozakulbarkhirb@gmail.com>
2023-09-21 10:43:18 +0700

    add navbar component

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex mx-auto md:w-2/4 w-full bg-gray-200 min-h-screen">
+    <Navbar />
+  </div>
+</template>
+
+<script setup>
+import Navbar from "./components/Navbar.vue";
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="w-full">
+    <nav class="py-4 px-4 shadow-md">
+      <h1 class="md:text-lg text-base font-semibold tracking-widest">
+        Notes App
+      </h1>
+    </nav>
+  </header>
+</template>
+
+<script setup></script>
